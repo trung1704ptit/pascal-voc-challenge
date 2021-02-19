adda46936260cda37ac0e168b2c89ff2c3ca53c3
Trung Nguyen <trung1704ptit@gmail.com>
2021-02-19 14:59:22 +0700

    making unique image id

--- counting_label.py
+++ counting_label.py
@@ -3,10 +3,10 @@ import csv
 import pandas as pd
 
 df = pd.read_csv(constants.TRAIN_LABEL_MAP_PATH)
-train_class_name_count = df.groupby("class_name")['image_id'].count()
+train_class_name_count = df.groupby("class_name")['image_id'].unique().count()
 
 df2 = pd.read_csv(constants.VAL_LABEL_MAP_PATH)
-val_class_name_count = df2.groupby("class_name")['image_id'].count()
+val_class_name_count = df2.groupby("class_name")['image_id'].unique().count()
 
 
 print(train_class_name_count)
